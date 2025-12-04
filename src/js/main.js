34d8447005138bf73af299b46ac51ae0cba864aa
EduardoGuereque <Jguereque_23@alu.uabcs.mx>
2025-12-04 06:47:26 -0700

    Delete src directory

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-async function loadComponent(id, file) {
-    const resp = await fetch(file);
-    const html = await resp.text();
-    document.getElementById(id).innerHTML = html;
-}
-
-loadComponent("header", "components/header.html");
-loadComponent("footer", "components/footer.html");
